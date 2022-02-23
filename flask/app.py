816c21e6670b4150050dc4ccb65b10126dba1cf7
Chris O'Brien <cobrien528@gmail.com>
2022-02-23 11:44:26 -0500

    flask activity

--- flask/app.py
+++ flask/app.py
@@ -1,20 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, request, session, render_template
 
 import random
 
 app = Flask(__name__)
 
-@app.route("/rand")
-def randomnumber():
-  i = random.randrange(100)
-  return render_template("lucky.html",number = i)
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def index():
-  return "<h1>Hello World from Chris O'Brien's computer !</h1>"
+  ip = request.environ['REMOTE_ADDR']
+  return render_template("home.html", ip = ip)
 
-@app.route("/coolsite")
+@app.route("/coolsite",methods=['GET','POST'])
 def coolsite():
-    return render_template("coolsite.html")
-    
+    message = ""
+    if request.method=="GET":
+        return render_template("coolsite.html")
+
+    else:
+        choice = request.form['choice']
+        if choice =="great":
+            message = "We're happy you love the website! If you have any comments  please click this link."
+        else:
+            message = "We're sorry you don't love the website! If you have any ideas for improvement  please click this link."
+
+
+
+
+        return render_template("coolsite.html", message=message)
+
+@app.route("/comments", methods=['GET', 'POST'])
+def comments():
+
+    if request.method=="GET":
+        return render_template("comments.html", message="")
+    else:
+        comment = request.form['comment']
+
+        print(comment)
+        message = "Your comment was: '" + comment + "'. Thank you for your feedback!"
+        return render_template("comments.html", message=message)
+
 app.run(host="127.0.0.1",port=5000,debug=True)
