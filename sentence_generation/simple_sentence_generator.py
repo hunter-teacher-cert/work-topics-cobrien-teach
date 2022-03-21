77cdaeba7854d81cfa86bf28b59447eb05ded824
Chris O'Brien <cobrien528@gmail.com>
2022-03-21 12:53:21 -0400

    modified simple sentence generator

--- sentence_generation/simple_sentence_generator.py
+++ sentence_generation/simple_sentence_generator.py
@@ -1,45 +1,58 @@
 import random
 
 articles = ["a", "the", "one", "some"]
-nouns = ["apple", "carrot", "rabbit", "banana", "basketball", "chess", "sun", "wind"]
-
+#removed Chess, this is a proper noun (note: cant say "I saw a chesss")
+nouns = ["apple", "carrot", "rabbit", "banana", "basketball", "sun", "wind"]
 #this list inlcudes both intransitive and transitive, should be decombosed
-verbs = ["cooks", "blows", "bounces", "walks", "jumps", "calls", "stays", "runs", "abides"]
+#removed stays, all other verbs in list are ambiguous between transitive and intransitive
+verbs = ["cooks", "blows", "bounces", "walks", "jumps", "calls", "runs", "abides"]
 #NOTE modified list of conjunctions 'for' is a preposition not a conjunction! 'nor' is complicated because of it's semantics ( it must appear in
 #the scope of a negation operator).
 conjunctions = ["and", "or", "but", "yet", "so"]
-dependent_clause_markers = ["although", "even if", "until", "when", "whether", "while", "in order to"]
+#removed "in order to", since this takes a verb phrase rather than sentential argument
+dependent_clause_markers = ["although", "even if", "until", "when", "whether", "while"]
 
 """
-Simple context-free Phrase Structure grammar
+Simple context-free  grammar for a fragment of English. "||" represents disjunction.
 
 simpleS -> NP VP
 NP -> article noun
-VP -> verb NP
-S -> simpleS conjunction simpleS || simpleS dependent_clause_marker simpleS || simpleS
-
+simpleVP -> verb NP || verb
+VP -> simpleVP + dependent_clause_marker + S || simpleVP + "in order to" + simpleVP || simpleVP
+S -> simpleS conjunction simpleS || simpleS
 
 
 
 """
 
-def simple_S():
+def simpleS():
     return NP() + " " + VP()
 
 
 def NP():
     return random.choice(articles) + " " + random.choice(nouns)
 
+def simpleVP():
+    my_choice = random.randrange(0,2)
+    if my_choice ==0:
+        return random.choice(verbs) + " "+  NP()
+    else :
+        return random.choice(verbs)
+
 def VP():
-    return random.choice(verbs) + " " + NP()
-def S():
     my_choice = random.randrange(0,3)
     if my_choice == 0:
-        return simple_S()() + " " + random.choice(conjunctions) + " " + simple_S()()
+        return simpleVP() + " " + random.choice(dependent_clause_markers) + " " + simpleS();
     elif my_choice == 1:
-        return simple_S()() + " " + random.choice(dependent_clause_markers) + " " + simple_S()()
+        return simpleVP() + " in order to " + simpleVP()
     else:
-        return simple_S()
+        return simpleVP()
+def S():
+    my_choice = random.randrange(0,2)
+    if my_choice == 0:
+        return simpleS() + " " + random.choice(conjunctions) + " " + simpleS()
+    else:
+        return simpleS()
 
 
 
